9b54e9b628e150311abc66ffbe736b93bb901806
Kiarash-Salehi <53015050+Kiarash-Salehi@users.noreply.github.com>
2022-03-29 22:18:29 +0430

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
 app.listen(process.env.PORT, () => console.log(`app is listening running on port ${process.env.PORT}`));
